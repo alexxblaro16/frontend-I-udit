fd25598bef5eb5587ebbda232b6ba3f1e0c0bec5
alexxblaro16 <alexxblaro@gmail.com>
2025-09-23 12:03:23 +0200

    Commit 2: Add button click event

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,8 @@
 // Commit 1: Basic Initialization
 console.log("¡Hola, mundo!");
+
+// Commit 2: Basic User Events
+const boton = document.getElementById("miBoton");
+boton.addEventListener("click", () => {
+  alert("¡Botón clickeado!");
+});
